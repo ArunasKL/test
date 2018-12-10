e97ff11ad7d4fe48684122a1b166ad2c03ed440b
ArunasKL <arunas.klimas@gmail.com>
2018-12-10 22:46:43 +0200

    papildymas after merge

--- New Python 1.py
+++ New Python 1.py
@@ -1,6 +1,21 @@
-import sys
+import numpy as np
 
-print(sys.builtin_module_names)
-print('nauja man tai ')
+x = np.random.random((64, 3, 32, 10))
+n = np.array(
+    [[[1, 2], [5, 6], [7, 8]],
+     [[5, 2], [5, 6], [7, 8]]]
 
-# papildymas
+)
+f = np.array([[2, 3],
+              [1, 2]])
+z = np.array([4, 5])
+b = np.dot(f, z)
+print(b)
+import keras
+import pandas
+
+print(pandas.__version__)
+import dask
+
+print(dask.__version__)
+print(keras.datasets)
